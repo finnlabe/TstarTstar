2bb154db4fe0219331774ab8e8f8cc58f5e6ae47
Finn <flabe@physnet.uni-hamburg.de>
2022-04-07 17:56:25 +0200

    Fix for variation hists

--- include/TstarTstarSignalRegionHists.h
+++ include/TstarTstarSignalRegionHists.h
@@ -26,62 +26,62 @@ public:
     uhh2::Event::Handle<double> h_ST;
 
     // weight handles
-    uhh2::Event::Handle<double> h_weight_puNominal;
-    uhh2::Event::Handle<double> h_weight_puUp;
-    uhh2::Event::Handle<double> h_weight_puDown;
+    uhh2::Event::Handle<float> h_weight_puNominal;
+    uhh2::Event::Handle<float> h_weight_puUp;
+    uhh2::Event::Handle<float> h_weight_puDown;
 
-    uhh2::Event::Handle<double> h_prefiringWeightNominal;
-    uhh2::Event::Handle<double> h_prefiringWeightUp;
-    uhh2::Event::Handle<double> h_prefiringWeightDown;
+    uhh2::Event::Handle<float> h_prefiringWeightNominal;
+    uhh2::Event::Handle<float> h_prefiringWeightUp;
+    uhh2::Event::Handle<float> h_prefiringWeightDown;
 
-    uhh2::Event::Handle<double> h_weight_btagdiscNominal;
+    uhh2::Event::Handle<float> h_weight_btagdiscNominal;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_jesUp;
-    uhh2::Event::Handle<double> h_weight_btagdisc_jesDown;
+    uhh2::Event::Handle<float> h_weight_btagdisc_jesUp;
+    uhh2::Event::Handle<float> h_weight_btagdisc_jesDown;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_lfUp;
-    uhh2::Event::Handle<double> h_weight_btagdisc_lfDown;
+    uhh2::Event::Handle<float> h_weight_btagdisc_lfUp;
+    uhh2::Event::Handle<float> h_weight_btagdisc_lfDown;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_hfUp;
-    uhh2::Event::Handle<double> h_weight_btagdisc_hfDown;
+    uhh2::Event::Handle<float> h_weight_btagdisc_hfUp;
+    uhh2::Event::Handle<float> h_weight_btagdisc_hfDown;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_hfstats1Up;
-    uhh2::Event::Handle<double> h_weight_btagdisc_hfstats1Down;
+    uhh2::Event::Handle<float> h_weight_btagdisc_hfstats1Up;
+    uhh2::Event::Handle<float> h_weight_btagdisc_hfstats1Down;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_hfstats2Up;
-    uhh2::Event::Handle<double> h_weight_btagdisc_hfstats2Down;
+    uhh2::Event::Handle<float> h_weight_btagdisc_hfstats2Up;
+    uhh2::Event::Handle<float> h_weight_btagdisc_hfstats2Down;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_lfstats1Up;
-    uhh2::Event::Handle<double> h_weight_btagdisc_lfstats1Down;
+    uhh2::Event::Handle<float> h_weight_btagdisc_lfstats1Up;
+    uhh2::Event::Handle<float> h_weight_btagdisc_lfstats1Down;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_lfstats2Up;
-    uhh2::Event::Handle<double> h_weight_btagdisc_lfstats2Down;
+    uhh2::Event::Handle<float> h_weight_btagdisc_lfstats2Up;
+    uhh2::Event::Handle<float> h_weight_btagdisc_lfstats2Down;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_cferr1Up;
-    uhh2::Event::Handle<double> h_weight_btagdisc_cferr1Down;
+    uhh2::Event::Handle<float> h_weight_btagdisc_cferr1Up;
+    uhh2::Event::Handle<float> h_weight_btagdisc_cferr1Down;
 
-    uhh2::Event::Handle<double> h_weight_btagdisc_cferr2Up;
-    uhh2::Event::Handle<double> h_weight_btagdisc_cferr2Down;
+    uhh2::Event::Handle<float> h_weight_btagdisc_cferr2Up;
+    uhh2::Event::Handle<float> h_weight_btagdisc_cferr2Down;
 
-    uhh2::Event::Handle<double> h_weight_sfelec_idNominal;
-    uhh2::Event::Handle<double> h_weight_sfelec_idUp;
-    uhh2::Event::Handle<double> h_weight_sfelec_idDown;
+    uhh2::Event::Handle<float> h_weight_sfelec_idNominal;
+    uhh2::Event::Handle<float> h_weight_sfelec_idUp;
+    uhh2::Event::Handle<float> h_weight_sfelec_idDown;
 
-    uhh2::Event::Handle<double> h_weight_sfelec_triggerNominal;
-    uhh2::Event::Handle<double> h_weight_sfelec_triggerUp;
-    uhh2::Event::Handle<double> h_weight_sfelec_triggerDown;
+    uhh2::Event::Handle<float> h_weight_sfelec_triggerNominal;
+    uhh2::Event::Handle<float> h_weight_sfelec_triggerUp;
+    uhh2::Event::Handle<float> h_weight_sfelec_triggerDown;
 
-    uhh2::Event::Handle<double> h_weight_sfmu_idNominal;
-    uhh2::Event::Handle<double> h_weight_sfmu_idUp;
-    uhh2::Event::Handle<double> h_weight_sfmu_idDown;
+    uhh2::Event::Handle<float> h_weight_sfmu_idNominal;
+    uhh2::Event::Handle<float> h_weight_sfmu_idUp;
+    uhh2::Event::Handle<float> h_weight_sfmu_idDown;
 
-    uhh2::Event::Handle<double> h_weight_sfmu_isolationNominal;
-    uhh2::Event::Handle<double> h_weight_sfmu_isolationUp;
-    uhh2::Event::Handle<double> h_weight_sfmu_isolationDown;
+    uhh2::Event::Handle<float> h_weight_sfmu_isolationNominal;
+    uhh2::Event::Handle<float> h_weight_sfmu_isolationUp;
+    uhh2::Event::Handle<float> h_weight_sfmu_isolationDown;
 
-    uhh2::Event::Handle<double> h_weight_sfmu_triggerNominal;
-    uhh2::Event::Handle<double> h_weight_sfmu_triggerUp;
-    uhh2::Event::Handle<double> h_weight_sfmu_triggerDown;
+    uhh2::Event::Handle<float> h_weight_sfmu_triggerNominal;
+    uhh2::Event::Handle<float> h_weight_sfmu_triggerUp;
+    uhh2::Event::Handle<float> h_weight_sfmu_triggerDown;
 
 };
 
--- src/TstarTstarSignalRegionHists.cxx
+++ src/TstarTstarSignalRegionHists.cxx
@@ -14,62 +14,62 @@ TstarTstarSignalRegionHists::TstarTstarSignalRegionHists(Context & ctx, const st
   h_ST = ctx.get_handle<double>("ST");
 
   // weight handles
-  h_weight_puNominal = ctx.get_handle<double>("weight_pu");
-  h_weight_puUp = ctx.get_handle<double>("weight_pu_up");
-  h_weight_puDown = ctx.get_handle<double>("weight_pu_down");
+  h_weight_puNominal = ctx.get_handle<float>("weight_pu");
+  h_weight_puUp = ctx.get_handle<float>("weight_pu_up");
+  h_weight_puDown = ctx.get_handle<float>("weight_pu_down");
 
-  h_prefiringWeightNominal = ctx.get_handle<double>("prefiringWeight");
-  h_prefiringWeightUp = ctx.get_handle<double>("prefiringWeightUp");
-  h_prefiringWeightDown = ctx.get_handle<double>("prefiringWeightDown");
+  h_prefiringWeightNominal = ctx.get_handle<float>("prefiringWeight");
+  h_prefiringWeightUp = ctx.get_handle<float>("prefiringWeightUp");
+  h_prefiringWeightDown = ctx.get_handle<float>("prefiringWeightDown");
 
-  h_weight_btagdiscNominal = ctx.get_handle<double>("weight_btagdisc_central");
+  h_weight_btagdiscNominal = ctx.get_handle<float>("weight_btagdisc_central");
 
-  h_weight_btagdisc_jesUp = ctx.get_handle<double>("weight_btagdisc_jes_up");
-  h_weight_btagdisc_jesDown = ctx.get_handle<double>("weight_btagdisc_jes_down");
+  h_weight_btagdisc_jesUp = ctx.get_handle<float>("weight_btagdisc_jes_up");
+  h_weight_btagdisc_jesDown = ctx.get_handle<float>("weight_btagdisc_jes_down");
 
-  h_weight_btagdisc_lfUp = ctx.get_handle<double>("weight_btagdisc_lf_up");
-  h_weight_btagdisc_lfDown = ctx.get_handle<double>("weight_btagdisc_lf_down");
+  h_weight_btagdisc_lfUp = ctx.get_handle<float>("weight_btagdisc_lf_up");
+  h_weight_btagdisc_lfDown = ctx.get_handle<float>("weight_btagdisc_lf_down");
 
-  h_weight_btagdisc_hfUp = ctx.get_handle<double>("weight_btagdisc_hf_up");
-  h_weight_btagdisc_hfDown = ctx.get_handle<double>("weight_btagdisc_hf_down");
+  h_weight_btagdisc_hfUp = ctx.get_handle<float>("weight_btagdisc_hf_up");
+  h_weight_btagdisc_hfDown = ctx.get_handle<float>("weight_btagdisc_hf_down");
 
-  h_weight_btagdisc_hfstats1Up = ctx.get_handle<double>("weight_btagdisc_hfstats1_up");
-  h_weight_btagdisc_hfstats1Down = ctx.get_handle<double>("weight_btagdisc_hfstats1_down");
+  h_weight_btagdisc_hfstats1Up = ctx.get_handle<float>("weight_btagdisc_hfstats1_up");
+  h_weight_btagdisc_hfstats1Down = ctx.get_handle<float>("weight_btagdisc_hfstats1_down");
 
-  h_weight_btagdisc_hfstats2Up = ctx.get_handle<double>("weight_btagdisc_hfstats2_up");
-  h_weight_btagdisc_hfstats2Down = ctx.get_handle<double>("weight_btagdisc_hfstats2_down");
+  h_weight_btagdisc_hfstats2Up = ctx.get_handle<float>("weight_btagdisc_hfstats2_up");
+  h_weight_btagdisc_hfstats2Down = ctx.get_handle<float>("weight_btagdisc_hfstats2_down");
 
-  h_weight_btagdisc_lfstats1Up = ctx.get_handle<double>("weight_btagdisc_lfstats1_up");
-  h_weight_btagdisc_lfstats1Down = ctx.get_handle<double>("weight_btagdisc_lfstats1_down");
+  h_weight_btagdisc_lfstats1Up = ctx.get_handle<float>("weight_btagdisc_lfstats1_up");
+  h_weight_btagdisc_lfstats1Down = ctx.get_handle<float>("weight_btagdisc_lfstats1_down");
 
-  h_weight_btagdisc_lfstats2Up = ctx.get_handle<double>("weight_btagdisc_lfstats2_up");
-  h_weight_btagdisc_lfstats2Down = ctx.get_handle<double>("weight_btagdisc_lfstats2_down");
+  h_weight_btagdisc_lfstats2Up = ctx.get_handle<float>("weight_btagdisc_lfstats2_up");
+  h_weight_btagdisc_lfstats2Down = ctx.get_handle<float>("weight_btagdisc_lfstats2_down");
 
-  h_weight_btagdisc_cferr1Up = ctx.get_handle<double>("weight_btagdisc_cferr1_up");
-  h_weight_btagdisc_cferr1Down = ctx.get_handle<double>("weight_btagdisc_cferr1_down");
+  h_weight_btagdisc_cferr1Up = ctx.get_handle<float>("weight_btagdisc_cferr1_up");
+  h_weight_btagdisc_cferr1Down = ctx.get_handle<float>("weight_btagdisc_cferr1_down");
 
-  h_weight_btagdisc_cferr2Up = ctx.get_handle<double>("weight_btagdisc_cferr2_up");
-  h_weight_btagdisc_cferr2Down = ctx.get_handle<double>("weight_btagdisc_cferr2_down");
+  h_weight_btagdisc_cferr2Up = ctx.get_handle<float>("weight_btagdisc_cferr2_up");
+  h_weight_btagdisc_cferr2Down = ctx.get_handle<float>("weight_btagdisc_cferr2_down");
 
-  h_weight_sfelec_idNominal = ctx.get_handle<double>("weight_sfelec_id");
-  h_weight_sfelec_idUp = ctx.get_handle<double>("weight_sfelec_id_up");
-  h_weight_sfelec_idDown = ctx.get_handle<double>("weight_sfelec_id_down");
+  h_weight_sfelec_idNominal = ctx.get_handle<float>("weight_sfelec_id");
+  h_weight_sfelec_idUp = ctx.get_handle<float>("weight_sfelec_id_up");
+  h_weight_sfelec_idDown = ctx.get_handle<float>("weight_sfelec_id_down");
 
-  h_weight_sfelec_triggerNominal = ctx.get_handle<double>("weight_sfelec_trigger");
-  h_weight_sfelec_triggerUp = ctx.get_handle<double>("weight_sfelec_trigger_up");
-  h_weight_sfelec_triggerDown = ctx.get_handle<double>("weight_sfelec_trigger_down");
+  //h_weight_sfelec_triggerNominal = ctx.get_handle<float>("weight_sfelec_trigger");
+  //h_weight_sfelec_triggerUp = ctx.get_handle<float>("weight_sfelec_trigger_up");
+  //h_weight_sfelec_triggerDown = ctx.get_handle<float>("weight_sfelec_trigger_down");
 
-  h_weight_sfmu_idNominal = ctx.get_handle<double>("weight_sfmu_id");
-  h_weight_sfmu_idUp = ctx.get_handle<double>("weight_sfmu_id_up");
-  h_weight_sfmu_idDown = ctx.get_handle<double>("weight_sfmu_id_down");
+  h_weight_sfmu_idNominal = ctx.get_handle<float>("weight_sfmu_id");
+  h_weight_sfmu_idUp = ctx.get_handle<float>("weight_sfmu_id_up");
+  h_weight_sfmu_idDown = ctx.get_handle<float>("weight_sfmu_id_down");
 
-  h_weight_sfmu_isolationNominal = ctx.get_handle<double>("weight_sfmu_isolation");
-  h_weight_sfmu_isolationUp = ctx.get_handle<double>("weight_sfmu_isolation_up");
-  h_weight_sfmu_isolationDown = ctx.get_handle<double>("weight_sfmu_isolation_down");
+  h_weight_sfmu_isolationNominal = ctx.get_handle<float>("weight_sfmu_isolation");
+  h_weight_sfmu_isolationUp = ctx.get_handle<float>("weight_sfmu_isolation_up");
+  h_weight_sfmu_isolationDown = ctx.get_handle<float>("weight_sfmu_isolation_down");
 
-  h_weight_sfmu_triggerNominal = ctx.get_handle<double>("weight_sfmu_trigger");
-  h_weight_sfmu_triggerUp = ctx.get_handle<double>("weight_sfmu_trigger_up");
-  h_weight_sfmu_triggerDown = ctx.get_handle<double>("weight_sfmu_trigger_down");
+  h_weight_sfmu_triggerNominal = ctx.get_handle<float>("weight_sfmu_trigger");
+  h_weight_sfmu_triggerUp = ctx.get_handle<float>("weight_sfmu_trigger_up");
+  h_weight_sfmu_triggerDown = ctx.get_handle<float>("weight_sfmu_trigger_down");
 
   // binning definition
   const int nbins = 34;
@@ -129,51 +129,42 @@ void TstarTstarSignalRegionHists::fill(const Event & event){
 
   if(debug) cout << "Starting Tstar Hists." << endl;
 
-  try { // this will of course only work once ST has been defined
 
-    double st = event.get(h_ST);
-    if(st > 6000) st = 5999.9; // handling overflow
+  double st = event.get(h_ST);
+  if(st > 6000) st = 5999.9; // handling overflow
 
-    // fill nominal
-    hist("pt_ST_nominal")->Fill(st, weight);
+  // fill nominal
+  hist("pt_ST_nominal")->Fill(st, weight);
 
-    // example dummy code
-    // double event_weight_up = event.get(h_some_variation_up);
-    // double event_weight_down = event.get(h_some_variation_down);
-    // hist("pt_ST_some_variation_up")->Fill(st, event_weight_up);
-    // hist("pt_ST_some_variation_down")->Fill(st, event_weight_down);
+  // pu
+  hist("pt_ST_puUp")->Fill(st, event.get(h_weight_puUp)*weight/event.get(h_weight_puNominal));
+  hist("pt_ST_puDown")->Fill(st, event.get(h_weight_puDown)*weight/event.get(h_weight_puNominal));
 
-    // pu
-    hist("pt_ST_puUp")->Fill(st, event.get(h_weight_puUp)*weight/event.get(h_weight_puNominal));
-    hist("pt_ST_puDown")->Fill(st, event.get(h_weight_puDown)*weight/event.get(h_weight_puNominal));
+  // prefiring
+  hist("pt_ST_prefiringUp")->Fill(st, event.get(h_prefiringWeightUp)*weight/event.get(h_prefiringWeightNominal));
+  hist("pt_ST_prefiringDown")->Fill(st, event.get(h_prefiringWeightDown)*weight/event.get(h_prefiringWeightNominal));
 
-    // prefiring
-    hist("pt_ST_prefiringUp")->Fill(st, event.get(h_prefiringWeightUp)*weight/event.get(h_prefiringWeightNominal));
-    hist("pt_ST_prefiringDown")->Fill(st, event.get(h_prefiringWeightDown)*weight/event.get(h_prefiringWeightNominal));
+  // put b-tagging stuff here
 
-    // put b-tagging stuff here
+  // sfelec_id
+  hist("pt_ST_sfelec_idUp")->Fill(st, event.get(h_weight_sfelec_idUp)*weight/event.get(h_weight_sfelec_idNominal));
+  hist("pt_ST_sfelec_idDown")->Fill(st, event.get(h_weight_sfelec_idDown)*weight/event.get(h_weight_sfelec_idNominal));
 
-    // sfelec_id
-    hist("pt_ST_sfelec_idUp")->Fill(st, event.get(h_weight_sfelec_idUp)*weight/event.get(h_weight_sfelec_idNominal));
-    hist("pt_ST_sfelec_idDown")->Fill(st, event.get(h_weight_sfelec_idDown)*weight/event.get(h_weight_sfelec_idNominal));
+  // sfelec_trigger
+  //hist("pt_ST_sfelec_triggerUp")->Fill(st, event.get(h_weight_sfelec_triggerUp)*weight/event.get(h_weight_sfelec_triggerNominal));
+  //hist("pt_ST_sfelec_triggerDown")->Fill(st, event.get(h_weight_sfelec_triggerDown)*weight/event.get(h_weight_sfelec_triggerNominal));
 
-    // sfelec_trigger
-    hist("pt_ST_sfelec_triggerUp")->Fill(st, event.get(h_weight_sfelec_triggerUp)*weight/event.get(h_weight_sfelec_triggerNominal));
-    hist("pt_ST_sfelec_triggerDown")->Fill(st, event.get(h_weight_sfelec_triggerDown)*weight/event.get(h_weight_sfelec_triggerNominal));
+  // sfmu_id
+  hist("pt_ST_sfmu_idUp")->Fill(st, event.get(h_weight_sfmu_idUp)*weight/event.get(h_weight_sfmu_idNominal));
+  hist("pt_ST_sfmu_idDown")->Fill(st, event.get(h_weight_sfmu_idDown)*weight/event.get(h_weight_sfmu_idNominal));
 
-    // sfmu_id
-    hist("pt_ST_sfmu_idUp")->Fill(st, event.get(h_weight_sfmu_idUp)*weight/event.get(h_weight_sfmu_idNominal));
-    hist("pt_ST_sfmu_idDown")->Fill(st, event.get(h_weight_sfmu_idDown)*weight/event.get(h_weight_sfmu_idNominal));
+  // sfmu_isolation
+  hist("pt_ST_sfmu_isolationUp")->Fill(st, event.get(h_weight_sfmu_isolationUp)*weight/event.get(h_weight_sfmu_isolationNominal));
+  hist("pt_ST_sfmu_isolationDown")->Fill(st, event.get(h_weight_sfmu_isolationDown)*weight/event.get(h_weight_sfmu_isolationNominal));
 
-    // sfmu_isolation
-    hist("pt_ST_sfmu_isolationUp")->Fill(st, event.get(h_weight_sfmu_isolationUp)*weight/event.get(h_weight_sfmu_isolationNominal));
-    hist("pt_ST_sfmu_isolationDown")->Fill(st, event.get(h_weight_sfmu_isolationDown)*weight/event.get(h_weight_sfmu_isolationNominal));
-
-    // sfmu_isolation
-    hist("pt_ST_sfmu_triggerUp")->Fill(st, event.get(h_weight_sfmu_triggerUp)*weight/event.get(h_weight_sfmu_triggerNominal));
-    hist("pt_ST_sfmu_triggerDown")->Fill(st, event.get(h_weight_sfmu_triggerDown)*weight/event.get(h_weight_sfmu_triggerNominal));
-
-  } catch(...) {}
+  // sfmu_isolation
+  //hist("pt_ST_sfmu_triggerUp")->Fill(st, event.get(h_weight_sfmu_triggerUp)*weight/event.get(h_weight_sfmu_triggerNominal));
+  //hist("pt_ST_sfmu_triggerDown")->Fill(st, event.get(h_weight_sfmu_triggerDown)*weight/event.get(h_weight_sfmu_triggerNominal));
 
   }
 
