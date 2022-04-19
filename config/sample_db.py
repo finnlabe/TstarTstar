344c9001dbf9d8ff3ae40020cdfb583d09be9904
Finn <flabe@physnet.uni-hamburg.de>
2022-04-19 10:51:22 +0200

    fixed data sample names

--- config/sample_db.py
+++ config/sample_db.py
@@ -1,39 +1,39 @@
 samplesDict = {
     #_________________________________________________
-    'DATA_SingleMuon_A': {
+    'DATA_SingleMuon_RunA': {
         'db_name': 'SingleMuon_RunA',
         'years': ['UL18']
     },
-    'DATA_SingleMuon_B': {
+    'DATA_SingleMuon_RunB': {
         'db_name': 'SingleMuon_RunB',
         'years': ['UL16preVFP', 'UL17', 'UL18']
     },
-    'DATA_SingleMuon_C': {
+    'DATA_SingleMuon_RunC': {
         'db_name': 'SingleMuon_RunC',
         'years': ['UL16preVFP', 'UL17', 'UL18']
     },
-    'DATA_SingleMuon_D': {
+    'DATA_SingleMuon_RunD': {
         'db_name': 'SingleMuon_RunD',
         'years': ['UL16preVFP', 'UL17', 'UL18']
     },
-    'DATA_SingleMuon_E': {
+    'DATA_SingleMuon_RunE': {
         'db_name': 'SingleMuon_RunE',
         'years': ['UL16preVFP', 'UL17']
     },
-    'DATA_SingleMuon_F': {
+    'DATA_SingleMuon_RunF': {
         'db_name': 'SingleMuon_RunF',
         'years': ['UL16preVFP', 'UL16postVFP', 'UL17']
     },
-    'DATA_SingleMuon_G': {
+    'DATA_SingleMuon_RunG': {
         'db_name': 'SingleMuon_RunG',
         'years': ['UL16postVFP']
     },
-    'DATA_SingleMuon_H': {
+    'DATA_SingleMuon_RunH': {
         'db_name': 'SingleMuon_RunH',
         'years': ['UL16postVFP']
     },
     #_________________________________________________
-    'DATA_SingleElectron_B': {
+    'DATA_SingleElectron_RunB': {
         'db_name': 'SingleElectron_RunB',
         'years': ['UL16preVFP', 'UL17']
     },
