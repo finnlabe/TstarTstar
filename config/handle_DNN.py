c10950a90e103dabeb885c665ccd2dde352d459a
Finn <flabe@physnet.uni-hamburg.de>
2022-12-02 11:46:32 +0100

    Handling scriupts

---
+++ config/handle_DNN.py
@@ -0,0 +1,14 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--option', required=True)
+parser.add_argument('--doData', action='store_true')
+args = parser.parse_args()
+
+years = ["UL16preVFP", "UL16postVFP", "UL17", "UL18"]
+
+for year in years:
+    os.system("sframe_batch.py -" + args.option + " config_DNN_" + year + "/parsedConfigFile_DNN_" + year + "_SM.xml")
+    os.system("sframe_batch.py -" + args.option + "  config_DNN_" + year + "/parsedConfigFile_DNN_" + year + "_Signal.xml")
+    if(args.doData): os.system("sframe_batch.py -" + args.option + "  config_DNN_" + year + "/parsedConfigFile_DNN_" + year + " _DATA.xml")
